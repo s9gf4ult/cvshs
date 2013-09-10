324d28b8b20ae741d30aabf65ca300c018ae6970
segfault <s9gf4ult@gmail.com>
2013-09-11 00:36:08 +0600

    fix

---
+++ randomsins.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+struct Comp {
+  double phase;
+  double period;
+  double amplitude;
+};
+
+int main(int argc, char **argv) {
+  
+
+
+}
--- randomsins.hs
+++ randomsins.hs
@@ -1,6 +1,8 @@
 module Main where
 
-
+import Control.Exception
+import Control.Applicative
+import Control.Monad
 import Control.DeepSeq
 import Control.Monad.Random
 import System.Random
@@ -13,12 +15,12 @@ amplitude = (0.1, 2)
 
 main = do
   g <- newStdGen
-  res <- evalRand g $ do
-      com <- U.fromList <$> replicateM compCount $ genSinComponent
-      spec1 <- U.fromList <$> replicateM compCount $ genSinComponent
-      spec2 <- U.fromList <$> replicateM compCount $ genSinComponent
-      spec3 <- U.fromList <$> replicateM compCount $ genSinComponent
-      spec4 <- U.fromList <$> replicateM compCount $ genSinComponent
+  res <- return $ (flip evalRand) g $ do
+      com <- U.fromList <$> (replicateM compCount $ genSinComponent)
+      spec1 <- com `seq` U.fromList <$> (replicateM compCount $ genSinComponent)
+      spec2 <- spec1 `seq` U.fromList <$> (replicateM compCount $ genSinComponent)
+      spec3 <- spec2 `seq` U.fromList <$> (replicateM compCount $ genSinComponent)
+      spec4 <- spec3 `seq` U.fromList <$> (replicateM compCount $ genSinComponent)
       return $ force (mergeComps com spec1,
                       mergeComps com spec2,
                       mergeComps com spec3,
@@ -27,18 +29,18 @@ main = do
   return ()
 
   where
-    genSinComponent :: Rand (U.Vector (Double, Double, Double))
+    genSinComponent :: (RandomGen g) => Rand g (Double, Double, Double)
     genSinComponent = do
       per <- getRandomR period
       amp <- getRandomR amplitude
       ph <- getRandomR (0, per)
       return (ph, per, amp)
     mergeComps :: U.Vector (Double, Double, Double) -> U.Vector (Double, Double, Double) -> U.Vector Double
-    mergeComps com spec = U.zipWith (\a b -> a + b + hi) (genSins com) (genSins spec)
+    mergeComps com spec = U.zipWith (\a b -> a + b) (genSins com) (genSins spec)
   
     genSins :: U.Vector (Double, Double, Double) -> U.Vector Double
-    genSins comps = U.fromList $ map (genSin comps) [0,2..fromIntegral len]
+    genSins comps = U.fromList $ map (genSin comps) [0..fromIntegral sinusLength]
 
     genSin :: U.Vector (Double, Double, Double) -> Double -> Double
-    genSin comps x = U.foldl' (\res (ph, (per, amp)) -> res + (amp * (sin $ (x/per*2*pi)+ph ))) 0 comps
+    genSin comps x = U.foldl' (\res (ph, per, amp) -> res + (amp * (sin $ (x/per*2*pi)+ph ))) 0 comps
   
